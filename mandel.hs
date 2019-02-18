1ff3f5bac46b3c9bd54c51a860dde90280ee22b3
ThomasM31 <43013914+ThomasM31@users.noreply.github.com>
2019-02-18 13:52:57 +0100

    Added code, working (without zoom or color)

--- mandel.hs
+++ mandel.hs
@@ -3,6 +3,8 @@ import Graphics.Gloss
 import Data.Word
 import Data.ByteString (ByteString, pack)
 
+---------------------------------------------------------------------------------------------------------------------------------
+
 purple :: [Word8]
 purple = [128, 0, 128, 64]
 
@@ -12,12 +14,13 @@ bitmapData = pack $ take 3999996 (cycle purple)
 ourPicture :: Picture
 ourPicture = bitmapOfByteString 1000 1000 (BitmapFormat TopToBottom PxRGBA) bitmapData True
 
-main = display (InWindow "Nice Window" (1000, 1000) (10, 10)) white ourPicture
+--main = display (InWindow "Nice Window" (1000, 1000) (10, 10)) white ourPicture
 
 ----------------------------------------------------------------------------------------------------------------------------------
 
 mandel :: RealFloat a => Complex a -> Complex a -> Complex a
 mandel z c = (z*z) + c
+
 
 pixelcheck :: RealFloat a => (Complex a -> Complex a -> Complex a) -> Int -> Int -> Complex a -> Complex a -> Int
 pixelcheck f current max_it z c
@@ -25,21 +28,28 @@ pixelcheck f current max_it z c
   | magnitude(z) > 2 = current
   | otherwise = pixelcheck f (current+1) max_it (f z c) c
 
+
 superpixelcheck :: RealFloat a => Complex a -> Int
 superpixelcheck z = pixelcheck mandel 0 255 z z
 
-cord_to_comp :: RealFloat a =>(a, a) -> Complex a
-cord_to_comp (x,y) = ((x/20) :+ (y/20))
 
-asd :: (Int, Int) -> [Int]
-asd (50,-51) = []
-asd (x,y) 
-    | x < 50 = iterations : asd (x+1,y)
-    | x == 50 = iterations : asd (-50, y-1)
-  where iterations = superpixelcheck (cord_to_comp (x,y))
+cord_to_comp :: RealFloat a => (a, a) -> a -> Complex a
+cord_to_comp (x,y) res = ((x/res) :+ (y/res))
 
-asdf :: [Int] -> [Word8]
-asdf (x:xs) = [255,0,0,(fromIntegral(x::Int)::Word8)] ++ asdf(xs)
 
-picture = bitmapOfByteString 100 100 (BitmapFormat TopToBottom PxRGBA) (pack (asdf( asd(-50,50) ) ) ) True
-main2 = display (InWindow "Nice Window" (100, 100) (10, 10)) black picture
+iterationList :: (Int, Int) -> [Int]
+iterationList (399,-401) = []
+iterationList (x,y) 
+    | x < 399 = iterations : iterationList (x+1,y)
+    | x == 399 = iterations : iterationList (-400, y-1)
+  where iterations = superpixelcheck (cord_to_comp ((fromIntegral x)::Float,(fromIntegral y)::Float) 200)
+
+
+createRGBA :: [Int] -> [Word8]
+createRGBA [] = []
+createRGBA (x:xs) = [0,0,0,(fromIntegral(x::Int)::Word8)] ++ createRGBA (xs)
+
+
+picture = bitmapOfByteString 800 800 (BitmapFormat TopToBottom PxRGBA) (pack (createRGBA (iterationList(-400,400)))) True
+main = display (InWindow "Nice Window" (800, 800) (10, 10)) white picture
+
