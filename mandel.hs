b7c43b223daec6ad867855eecb234cb515ec9e32
ThomasM31 <43013914+ThomasM31@users.noreply.github.com>
2019-02-20 11:42:09 +0100

    with some function descriptions
    Detta library verkar kunna skapa gradients: http://hackage.haskell.org/package/Rasterific-0.7.4.2/docs/Graphics-Rasterific-Texture.html
    
    Also andra länkar: 
    https://en.wikipedia.org/wiki/Mandelbrot_set
    http://hackage.haskell.org/package/gloss-1.13.0.1/docs/Graphics-Gloss-Data-Color.html

--- mandel.hs
+++ mandel.hs
@@ -2,33 +2,33 @@ import Data.Complex
 import Graphics.Gloss
 import Data.Word
 import Data.ByteString (ByteString, pack)
+--import Graphics.Rasterific.Texture
+
 
 {- mandel z c 
-   DESCRIPTION: 
-   PRE: 
-   RETURNS: 
-   EXAMPLES: 
-   VARIANT: 
+   DESCRIPTION: Calculates one iteration of the formula for numbers in the Mandelbrot set
+   PRE: TRUE
+   RETURNS: A number in the Mandelbrot set, based on z and c
+   EXAMPLES: mandel (4 :+ 2) (2 :+ 3) -> 14.0 :+ 19.0
 -}
 mandel :: RealFloat a => Complex a -> Complex a -> Complex a
 mandel z c = (z*z) + c
 
 {- pixelCheck z  
-   DESCRIPTION: 
-   PRE: 
-   RETURNS: 
-   EXAMPLES: 
-   VARIANT: 
+   DESCRIPTION: Calculates how many iterations of the Mandelbrot formula it takes for z to ?????????????????????????????????????
+   PRE: TRUE
+   RETURNS: The number of iterations it takes for z to grow beyond the size of what is allowed in a Mandelbrot set
+   EXAMPLES: pixelCheck (0.05 :+ 0.9) -> 5
 -}
 pixelCheck :: RealFloat a => Complex a -> Int
 pixelCheck z = pixelCheckAux mandel 0 255 z z
 
 {- pixelCheckAux f currIt maxIt z c
-   DESCRIPTION: 
-   PRE: 
-   RETURNS: 
-   EXAMPLES: 
-   VARIANT: 
+   DESCRIPTION: Calculates how many iterations of f it takes for z to ???????????????????????????????????????????????????????????
+   PRE: TRUE
+   RETURNS: currIt, i.e. the number of iterations passed
+   EXAMPLES: pixelCheckAux mandel 0 255 (0.05 :+ 0.9) (0.05 :+ 0) -> 255
+   VARIANT: ???????????????????????????????????????????????????????????
 -}
 pixelCheckAux :: RealFloat a => (Complex a -> Complex a -> Complex a) -> Int -> Int -> Complex a -> Complex a -> Int
 pixelCheckAux f currIt maxIt z c
@@ -37,41 +37,43 @@ pixelCheckAux f currIt maxIt z c
   | otherwise = pixelCheckAux f (currIt+1) maxIt (f z c) c
 
 {- cordToComp (x,y) res
-   DESCRIPTION: 
-   PRE: 
-   RETURNS: 
-   EXAMPLES: 
-   VARIANT: 
+   DESCRIPTION: Creates a complex number based on the coordinates (x,y), where x and y are specific pixels
+   PRE: res is positive ????????????????????????????????????????????????????????????????????
+   RETURNS: A comlex number representing coordinates
+   EXAMPLES: cordToComp (250,100) 400 -> 0.625 :+ 0.25
 -}
 cordToComp :: RealFloat a => (a, a) -> a -> Complex a
 cordToComp (x,y) res = ((x/res) :+ (y/res))
 
 {- iterationList (x,y)
    DESCRIPTION: 
-   PRE: 
+   PRE: x < 399, y > -401 ??????????????????????????????????????????
    RETURNS: 
    EXAMPLES: 
-   VARIANT: 
+   VARIANT: y ?????????????????????????????????????????????????
 -}
 iterationList :: (Int, Int) -> [Int]
-iterationList (499,-501) = []
+iterationList (399,-401) = []
 iterationList (x,y) 
     | x < 399 = iterations : iterationList (x+1,y)
-    | x == 399 = iterations : iterationList (-500, y-1)
-  where iterations = pixelCheck (cordToComp ((fromIntegral x)::Float,(fromIntegral y)::Float) 500)
+    | x == 399 = iterations : iterationList (-400, y-1)
+  where iterations = pixelCheck (cordToComp ((fromIntegral x)::Float,(fromIntegral y)::Float) 200)
 
 {- createRGBA ls
-   DESCRIPTION: 
-   PRE: 
-   RETURNS: 
-   EXAMPLES: 
-   VARIANT: 
+   DESCRIPTION: Takes a list of integers and converts them to shades of black (i.e. the Alpha of RGBA)
+   PRE: (x:xs) only contain positive integers ???????????????????????????????????????????????????????????????????????????????
+   RETURNS: A list of shades of black, where Alpha is the number of iterations passed through the Mandelbrot formula
+   EXAMPLES: createRGBA [1,50,244] -> [0,0,0,1,0,0,0,50,0,0,0,244]
+   VARIANT: length (x:xs)
 -}
 createRGBA :: [Int] -> [Word8]
 createRGBA [] = []
 createRGBA (x:xs) = [0,0,0,(fromIntegral(x::Int)::Word8)] ++ createRGBA (xs)
 
 
-picture = bitmapOfByteString 1000 1000 (BitmapFormat TopToBottom PxRGBA) (pack (createRGBA (iterationList (-500,500)))) True
-main = display (InWindow "Nice Window" (1000, 1000) (10, 10)) black picture
+
+--------------------------------------------------------------------------------------------------------------------------------------
+
+picture = bitmapOfByteString 800 800 (BitmapFormat TopToBottom PxRGBA) (pack (createRGBA (iterationList (-400,400)))) True
+main = display (InWindow "Epic Gamer Window" (800, 800) (10, 10)) white picture
 
