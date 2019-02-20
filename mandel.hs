78ee8e1f5a612fe528c5237e2828efd98ed2a4b4
ThomasM31 <43013914+ThomasM31@users.noreply.github.com>
2019-02-20 10:28:16 +0100

    Main without gradient/zoom, updated funciton names

--- mandel.hs
+++ mandel.hs
@@ -3,53 +3,75 @@ import Graphics.Gloss
 import Data.Word
 import Data.ByteString (ByteString, pack)
 
----------------------------------------------------------------------------------------------------------------------------------
-
-purple :: [Word8]
-purple = [128, 0, 128, 64]
-
-bitmapData :: ByteString
-bitmapData = pack $ take 3999996 (cycle purple)
-
-ourPicture :: Picture
-ourPicture = bitmapOfByteString 1000 1000 (BitmapFormat TopToBottom PxRGBA) bitmapData True
-
---main = display (InWindow "Nice Window" (1000, 1000) (10, 10)) white ourPicture
-
-----------------------------------------------------------------------------------------------------------------------------------
-
+{- mandel z c 
+   DESCRIPTION: 
+   PRE: 
+   RETURNS: 
+   EXAMPLES: 
+   VARIANT: 
+-}
 mandel :: RealFloat a => Complex a -> Complex a -> Complex a
 mandel z c = (z*z) + c
 
+{- pixelCheck z  
+   DESCRIPTION: 
+   PRE: 
+   RETURNS: 
+   EXAMPLES: 
+   VARIANT: 
+-}
+pixelCheck :: RealFloat a => Complex a -> Int
+pixelCheck z = pixelCheckAux mandel 0 255 z z
 
-pixelcheck :: RealFloat a => (Complex a -> Complex a -> Complex a) -> Int -> Int -> Complex a -> Complex a -> Int
-pixelcheck f current max_it z c
-  | current >= max_it = current
-  | magnitude(z) > 2 = current
-  | otherwise = pixelcheck f (current+1) max_it (f z c) c
+{- pixelCheckAux f currIt maxIt z c
+   DESCRIPTION: 
+   PRE: 
+   RETURNS: 
+   EXAMPLES: 
+   VARIANT: 
+-}
+pixelCheckAux :: RealFloat a => (Complex a -> Complex a -> Complex a) -> Int -> Int -> Complex a -> Complex a -> Int
+pixelCheckAux f currIt maxIt z c
+  | currIt >= maxIt = currIt
+  | magnitude(z) > 2 = currIt
+  | otherwise = pixelCheckAux f (currIt+1) maxIt (f z c) c
 
+{- cordToComp (x,y) res
+   DESCRIPTION: 
+   PRE: 
+   RETURNS: 
+   EXAMPLES: 
+   VARIANT: 
+-}
+cordToComp :: RealFloat a => (a, a) -> a -> Complex a
+cordToComp (x,y) res = ((x/res) :+ (y/res))
 
-superpixelcheck :: RealFloat a => Complex a -> Int
-superpixelcheck z = pixelcheck mandel 0 255 z z
-
-
-cord_to_comp :: RealFloat a => (a, a) -> a -> Complex a
-cord_to_comp (x,y) res = ((x/res) :+ (y/res))
-
-
+{- iterationList (x,y)
+   DESCRIPTION: 
+   PRE: 
+   RETURNS: 
+   EXAMPLES: 
+   VARIANT: 
+-}
 iterationList :: (Int, Int) -> [Int]
-iterationList (399,-401) = []
+iterationList (499,-501) = []
 iterationList (x,y) 
     | x < 399 = iterations : iterationList (x+1,y)
-    | x == 399 = iterations : iterationList (-400, y-1)
-  where iterations = superpixelcheck (cord_to_comp ((fromIntegral x)::Float,(fromIntegral y)::Float) 200)
+    | x == 399 = iterations : iterationList (-500, y-1)
+  where iterations = pixelCheck (cordToComp ((fromIntegral x)::Float,(fromIntegral y)::Float) 500)
 
-
+{- createRGBA ls
+   DESCRIPTION: 
+   PRE: 
+   RETURNS: 
+   EXAMPLES: 
+   VARIANT: 
+-}
 createRGBA :: [Int] -> [Word8]
 createRGBA [] = []
 createRGBA (x:xs) = [0,0,0,(fromIntegral(x::Int)::Word8)] ++ createRGBA (xs)
 
 
-picture = bitmapOfByteString 800 800 (BitmapFormat TopToBottom PxRGBA) (pack (createRGBA (iterationList(-400,400)))) True
-main = display (InWindow "Nice Window" (800, 800) (10, 10)) white picture
+picture = bitmapOfByteString 1000 1000 (BitmapFormat TopToBottom PxRGBA) (pack (createRGBA (iterationList (-500,500)))) True
+main = display (InWindow "Nice Window" (1000, 1000) (10, 10)) black picture
 
